ae1e29c2995d388850b5f9963d03b256f020026f
İmdat Gürbüz <grbzimdatofficial@gmail.com>
2023-04-22 15:12:35 +0300

    img added

--- app-gui.py
+++ app-gui.py
@@ -1,8 +1,16 @@
 import tkinter as tk
+from PIL import Image
+
 
 screen = tk.Tk()
 screen.minsize(360,600)
 screen.title("Secret Notes")
+
+#? top-secret img
+canvas = tk.Canvas(screen, width=360,height=200)
+canvas.pack()
+logo = tk.PhotoImage(file="top-secret2.png")
+canvas.create_image((180,100),image=logo)
 
 
 
